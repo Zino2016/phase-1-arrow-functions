15f4dbbc046622847e8a63d6edcfb86a09287a57
Jessica <jwhi230@icloud.com>
2023-04-10 13:55:47 -0500

    complete

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (divide) => {
+    let a = 2000
+    let b = 100
+    return (a/b)
+ 
+}
+const square = (multiplies) => {
+    let x = 2 
+    return (x*x)
+   
+}
+const add = (addition) => {
+    let a = 3
+    let b = 4
+    return (a+b)
+
+}
